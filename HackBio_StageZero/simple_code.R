d3d7b2fdab2117f2e17ef89dd4a0c186d3d943d1
Bayo Obafemi <67081020+bayooba@users.noreply.github.com>
2026-01-24 00:18:47 +0100

    changed the cat() funtion to paste() in the sample_code.R file

--- HackBio_StageZero/simple_code.R
+++ HackBio_StageZero/simple_code.R
@@ -3,4 +3,6 @@ institution <- "University of Bologna"
 favorite_gene <- "aco"
 organism_of_favorite_gene <- "ethylene-procuding plants"
 
-cat("Hi, my name is ", name, ", a PhD candidate at the ", institution, ". My favorite gene is ", favorite_gene, " in ", organism_of_favorite_gene,".", sep = "")
+output <- paste0("Hi, my name is ",name,", a PhD candidate at the ", institution,". My favorite gene is ",favorite_gene," in ", organism_of_favorite_gene,".")
+
+print(output)
