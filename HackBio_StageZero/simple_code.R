aaf46eacbaa69dac803b6a456e190450f3435f0f
Bayo Obafemi <67081020+bayooba@users.noreply.github.com>
2026-01-23 15:04:50 +0100

    added the files for stagezero tasks

---
+++ HackBio_StageZero/simple_code.R
@@ -0,0 +1,6 @@
+name <- "Adebayo Obafemi"
+institution <- "University of Bologna"
+favorite_gene <- "aco"
+organism_of_favorite_gene <- "ethylene-procuding plants"
+
+cat("Hi, my name is ", name, ", a PhD candidate at the ", institution, ". My favorite gene is ", favorite_gene, " in ", organism_of_favorite_gene,".", sep = "")
